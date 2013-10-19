f7b65928638e008f6a08ed2ea4451e9c3d098383
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2013-10-19 14:27:27 -0300

    Biblioteca c/ filtros

---
+++ filters.py
@@ -0,0 +1,28 @@
+from audiolazy import *
+
+def low_pass (stream):
+    pass
+
+def high_pass (stream):
+    pass
+
+def ressonator (stream):
+    pass
+
+def limiter (stream):
+    pass
+
+def compressor (stream):
+    pass
+
+def expander(stream):
+    pass
+
+def distortion (stream):
+    pass
+
+def phaser (stream):
+    pass
+
+def flanger (stream):
+    pass
