5f31dc3088fc0ed21e68593a981cbc69aad9377b
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2013-10-19 16:25:25 -0300

    mudou

--- main.py
+++ main.py
@@ -6,14 +6,26 @@ import filters
 from audiolazy import *
 from scipy.io import wavfile
 
-filename = "notas/do.wav"#raw_input("Type your .wav file location: ")
+
+filename = "notas/si.wav"#raw_input("Type your .wav file location: ")
 myrate, data_sound = wavfile.read(filename)
 print myrate
 s, Hz = sHz(myrate)
-data = Stream(data_sound)/max(abs(data_sound.max()),abs(data_sound.min()))
-player = AudioIO()
+data = Stream(data_sound)
+player = AudioIO(False)
 #player.play(data)
 
-filt = filters.low_pass(1/8.0)
-data2 = filt(data)
-player.play(data2)
+filt = 1 / (1 - z*pi/2)
+filt2 = (1 + z*pi/2) / (1 - z*pi/2)
+filt.plot(mag_scale="linear")
+filt2.plot(mag_scale="linear")
+data = thub(filt(data),3)
+maximo1 = abs(max(data))
+maximo2 = abs(min(data))
+maximo = max(maximo1,maximo2)
+print maximo1,maximo2,maximo
+data = data / (maximo*1.0)
+player.play(data)
+#stop = raw_input("Press ENTER to stop ")
+#player.close()
+
