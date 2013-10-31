7ed70ccde3d912543d71dc23e97d4b3c351495d1
RicardoBoccoliGallego <ricardo.gallego93@gmail.com>
2013-10-31 21:37:10 -0200

    gui.py alterado

--- gui.py
+++ gui.py
@@ -11,42 +11,42 @@ def sair(e):
     
 class main_window(wx.Frame):
     def __init__(self, *args, **kwargs):
-        super(Example, self).__init__(*args, **kwargs) 
+        super(main_window, self).__init__(*args, **kwargs) 
         self.InitUI()
         
     def InitUI(self):    
-
+        #Cria o menu        
         menubar = wx.MenuBar()
-        fileMenu = wx.Menu()
-        fitem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
-        menubar.Append(fileMenu, '&File')
+        menu = wx.Menu()
+        item1 = menu.Append(wx.ID_NEW, "Novo Preset", "Criar novo preset de filtros")
+        item2 = menu.Append(wx.ID_OPEN, "Carregar Preset", "Carregar preset de filtros")
+        item3 = menu.Append(wx.ID_EXIT, "Sair", "Sair do aplicativo")
+        menubar.Append(menu, 'Arquivo')
         self.SetMenuBar(menubar)
-        
-        self.Bind(wx.EVT_MENU, self.OnQuit, fitem)
+        self.Bind(wx.EVT_MENU, self.OnNewPreset, item1)
+        self.Bind(wx.EVT_MENU, self.OnLoadPreset, item2)
+        self.Bind(wx.EVT_MENU, self.OnQuit, item3)
+        #Cria os panels que contem os filtros
+        filter_list(self,"teste")
 
-        self.SetSize((300, 200))
-        self.SetTitle('Simple menu')
-        self.Centre()
-        self.Show(True)
-        
+
+
+        self.Show(True)    
+    
     def OnQuit(self, e):
+        self.Close()
+        
+    def OnNewPreset(self, e):
+        self.Close()
+    
+    def OnLoadPreset(self, e):
         self.Close()
 def create_main_window():
     """
     Function that creates the main window of the Digital Pedalboard program
     """  
     app = wx.App()
-    window = wx.Frame(None,title="Digital Pedalboard",name="main_window")
-    menubar = wx.MenuBar()
-    menu = wx.Menu()
-    item1 = menu.Append(wx.ID_NEW, "Novo Preset", "Criar novo preset de filtros")
-    item2 = menu.Append(wx.ID_OPEN, "Carregar Preset", "Carregar preset de filtros")
-    item3 = menu.Append(wx.ID_EXIT, "Sair", "Sair do aplicativo")
-    menubar.Append(menu,"Executar")
-    window.SetMenuBar(menubar)
-    window.Bind(wx.EVT_MENU, sair, item3)
-    window.Maximize()   
-    window.Show()
+    window = main_window(None,title="Digital Pedalboard",name="main_window")
     app.MainLoop()
     return window
 
@@ -61,11 +61,12 @@ def filter_checkbox(check_list, name):
     """
     pass
 
-def filter_list(name):
+def filter_list(window, name):
     """
     Create a checkbox filter container.     
     """
-    pass
+    panel = wx.Panel(window, -1)
+    wx.TextCtrl(panel,pos=(3,3), size=(250,150))
 
 def filter_window():
     """
@@ -73,4 +74,4 @@ def filter_window():
     """
     pass
 
-main_window()
+window = create_main_window()
