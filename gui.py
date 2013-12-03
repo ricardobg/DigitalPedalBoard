099eb393118878d4865e9c43735a904b3533bb01
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2013-12-03 11:25:54 -0200

    License added

--- gui.py
+++ gui.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# This file is part of DigitalPedalBoard python program.
+# Copyright (C) 2013 Copyright (C) 2013 Daniel Ken Fujimori Killner,
+# Gabriel Moura Vieira Martinez, Rafael Alves de Araujo Sena,
+# Ricardo Boccoli Gallego, Danilo de Jesus da Silva Bellini.
+#
+# DigitalPedalBoard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 """
 File to create windows and widgets
 
